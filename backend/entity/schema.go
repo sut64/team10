ff61f04e9afa89d0eafd8c44025759b4880e7f09
Poramee Suriyajanno <pookoak8501@gmail.com>
2022-01-28 01:46:55 +0700

    close #81 - add entity

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -37,3 +37,10 @@ type DrugAllergy struct {
 	Name    string
 	Symptom string
 }
+
+type Gender struct {
+	gorm.Model
+	Genders string `gorm:"uniqueIndex"`
+	//1 Gender เป็นเจ้าของได้หลาย Personnel_ID
+	Personnels []Personnel `gorm:"foreignKey:GenderID"`
+}
