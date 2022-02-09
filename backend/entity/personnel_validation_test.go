cbe4ccf5ad806335b41224855f2b6da860bba152
Poramee Suriyajanno <pookoak8501@gmail.com>
2022-02-10 02:37:50 +0700

    close #206 - แก้ไข Unit test สำหรับ Personnel

--- backend/entity/personnel_validation_test.go
+++ backend/entity/personnel_validation_test.go
@@ -34,8 +34,8 @@ func TestPersonalIDMustBeInValidPattern(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	personnel := Personnel{
-		Name:       "Poramee Suriyajanno",
-		Personalid: "1104200227093",
+		Name:       "Poramee",
+		Personalid: "11042002270934X", // fail มีตัวอักษรที่นอกเหนือจาอก 0-9 อยู่ข้างใน
 		BirthDay:   time.Date(2001, 8, 2, 0, 0, 0, 0, time.UTC),
 		Tel:        "0638267373",
 		Address:    "889 หมู่ 1 ต.ระแงง อ.ศีขรภูมิ จ.สุรินทร์ 32110",
@@ -44,12 +44,14 @@ func TestPersonalIDMustBeInValidPattern(t *testing.T) {
 
 	ok, err := govalidator.ValidateStruct(personnel)
 
-	// ok ต้องเป็น true แปลว่าไม่มี error
-	g.Expect(ok).To(BeTrue())
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-	// err เป็นค่า nil แปลว่าไม่มี error
-	g.Expect(err).To(BeNil())
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกบัตรประจำตัวประชาชนให้ถูกต้อง"))
 }
 
 func TestBirthDayMustBePast(t *testing.T) {
@@ -57,42 +59,47 @@ func TestBirthDayMustBePast(t *testing.T) {
 
 	// ข้อมูลถูกต้องหมดทุก field
 	personnel := Personnel{
-		Name:       "Poramee Suriyajanno",
+		Name:       "Poramee",
 		Personalid: "1104200227093",
-		BirthDay:   time.Date(2001, 8, 2, 0, 0, 0, 0, time.UTC),
-		Tel:        "0638267373",
-		Address:    "889 หมู่ 1 ต.ระแงง อ.ศีขรภูมิ จ.สุรินทร์ 32110",
-		Salary:     15000,
+		//BirthDay:   time.Date(2001, 8, 2, 0, 0, 0, 0, time.UTC),
+		BirthDay: time.Now().Add(24 * time.Hour), // อนาคต, fail
+		Tel:      "0638267373",
+		Address:  "889 หมู่ 1 ต.ระแงง อ.ศีขรภูมิ จ.สุรินทร์ 32110",
+		Salary:   15000,
 	}
 
 	// ตรวจสอบด้วย govalidator
 	ok, err := govalidator.ValidateStruct(personnel)
 
-	// ok ต้องเป็น true แปลว่าไม่มี error
-	g.Expect(ok).To(BeTrue())
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-	// err เป็นค่า nil แปลว่าไม่มี error
-	g.Expect(err).To(BeNil())
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณากรอกวันเกิดให้ถูกต้อง"))
 }
 
 func TestSalaryMustBePositive(t *testing.T) {
 	g := NewGomegaWithT(t)
 	personnel := Personnel{
-		Name:       "Poramee Suriyajanno",
+		Name:       "Poramee",
 		Personalid: "1104200227093",
 		BirthDay:   time.Date(2001, 8, 2, 0, 0, 0, 0, time.UTC),
 		Tel:        "0638267373",
 		Address:    "889 หมู่ 1 ต.ระแงง อ.ศีขรภูมิ จ.สุรินทร์ 32110",
-		Salary:     15000,
+		Salary:     -50,
 	}
 
 	ok, err := govalidator.ValidateStruct(personnel)
 
-	// ok ต้องเป็น true แปลว่าไม่มี error
-	g.Expect(ok).To(BeTrue())
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
 
-	// err เป็นค่า nil แปลว่าไม่มี error
-	g.Expect(err).To(BeNil())
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
 
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณาเงินเดือนให้มีค่าเป็นบวก"))
 }
